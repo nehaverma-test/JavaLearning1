f3035805ad0dcee60c715e5f40e5c58b04c86dfb
nehaverma-test <67279749+nehaverma-test@users.noreply.github.com>
2024-07-09 14:56:34 +0530

    Code using Equals Method to compare 2 Strings

---
+++ src/testing/com/ComparisonOfStrings.java
@@ -0,0 +1,17 @@
+package testing.com;
+
+public class ComparisonOfStrings {
+
+    public static void main(String[] args) {
+        String str1 = "TEST";
+        String str2 = "test";
+
+        //Equals method check whether 2 strings are equal or not
+
+        Boolean flag1 = str1.equals(str2);
+        System.out.println(flag1);
+
+
+
+    }
+}
