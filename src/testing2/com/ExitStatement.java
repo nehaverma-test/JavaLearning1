e0489133aaa3f4a718a426ea3c3a6cebcb3ad748
nehaverma-test <67279749+nehaverma-test@users.noreply.github.com>
2024-07-18 14:44:56 +0530

    Code to show break example

---
+++ src/testing2/com/ExitStatement.java
@@ -0,0 +1,16 @@
+package testing2.com;
+
+public class ExitStatement {
+    public static void main(String[] args) {
+        for(int i =0;i<5;i++)
+        {
+            if(i==3)
+                break;
+            System.out.println(i);
+        }
+        //When i = 3 , control gets transferred to immediate statement after the loop
+        System.out.println("After loop");
+
+    }
+
+}
