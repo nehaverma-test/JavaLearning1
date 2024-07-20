5c81cc4abb865c4f75de0bd754a4ae4f62bdb518
nehaverma-test <67279749+nehaverma-test@users.noreply.github.com>
2024-07-20 21:11:45 +0530

    Code to show Switch Case example

---
+++ src/testing2/com/SwitchCaseExample.java
@@ -0,0 +1,21 @@
+package testing2.com;
+
+public class SwitchCaseExample {
+    public static void main(String[] args) {
+
+        int a = 10;
+        switch(-1) {
+            default:
+                System.out.println("default");
+                break;
+            case -1:
+                System.out.println("A");
+                break;
+
+            case 15:
+            System.out.println("B");
+            break;
+        }
+
+    }
+}
