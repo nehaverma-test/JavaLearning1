958de3867719adca076d89685f8c177449a8616e
nehaverma-test <67279749+nehaverma-test@users.noreply.github.com>
2024-07-20 20:59:59 +0530

    Code to show If Else statement based on Boolean value

---
+++ src/testing2/com/IfElseBasedOnBooleanValue.java
@@ -0,0 +1,21 @@
+package testing2.com;
+
+public class IfElseBasedOnBooleanValue {
+    public static void main(String[] args) {
+
+   if(true)
+   {
+       System.out.println("A");
+       if(true)
+       {
+           System.out.println("B");
+           if(true)
+           {
+               System.out.println("C");
+           }
+       }
+
+   }
+
+    }
+}
